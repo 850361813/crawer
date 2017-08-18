ff5a2e378a37b3cf4ae597664489f025c84e6173
zhaoxianghui <zhaoxianghui@baidu.com>
2017-08-18 14:39:18 +0800

    fix

--- common/google_service.py
+++ common/google_service.py
@@ -104,17 +104,13 @@ def google_translate_DtoE(to_translate, from_language="de", to_language="en"):
 
 
 def get_geo_for_address(address):
-    headers = {
-        'Upgrade-Insecure-Requests':'1',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-    }
+
     position_dict = dict()
     quoto_address = urllib2.quote(address, ':?=/')
     address_url = 'https://maps.googleapis.com/maps/api/geocode/json?address='+quoto_address
     print address_url
     result = urllib2.urlopen(address_url).read().decode('utf-8')
 
-    print result
     # 中文url需要转码才能识别
     # response = result.decode('utf-8')
     response_json = json.loads(result)
